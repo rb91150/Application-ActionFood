09f98327ef2a6a0ece62967338794b0e7f1d784b
rb91150 <raphael.boeebosale.pro@gmail.com>
2025-01-15 15:44:23 +0100

    reception

---
+++ reception.js
@@ -0,0 +1,15 @@
+// Ajout d'un comportement pour les boutons d'aide
+
+document.querySelector('.help-yes').addEventListener('click', () => {
+
+    alert('Redirection vers le formulaire d\'aide.');
+
+});
+
+
+
+document.querySelector('.help-no').addEventListener('click', () => {
+
+    alert('Aucun contact requis.');
+
+});
